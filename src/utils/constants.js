8bd4a6717679f19626766cf22723c7e96a710b93
jacrifla <jaquelini_cris@hotmail.com>
2025-04-24 23:33:18 -0300

    api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
-export const BASE_URL = 'http://localhost:4000';
+// export const BASE_URL = 'http://localhost:4000';
+export const BASE_URL = 'https://server-filmes-production.up.railway.app';
 export const API_KEY = '57d80942fbc1daf9cd1429ea9c9ee8f5';
